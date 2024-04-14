a8ee8c9a46dd7491ae3e31db2d429d48f0c13224
anujcode17 <anujcode17@proton.me>
2024-04-14 23:42:03 +0530

    added random color project and accordian project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Accordian from "./components/accpordian";
+import RandomColor from "./components/random-color";
 
 function App() {
   return (
     <div className="App">
-      <Accordian />
+      {/* <Accordian /> */}
+      <RandomColor />
     </div>
   );
 }
